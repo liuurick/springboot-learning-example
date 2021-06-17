d66bfc65f0587b3a620badbd6f00531843d45f7c
liuurick <liuurick@gmail.com>
2021-06-17 09:22:25 +0800

    update

---
+++ springboot-aop-weblog/src/main/java/com/liuurick/weblog/SpringbootAopWeblogApplication.java
@@ -0,0 +1,13 @@
+package com.liuurick.weblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAopWeblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAopWeblogApplication.class, args);
+    }
+
+}
---
+++ springboot-aop-weblog/src/test/java/com/liuurick/weblog/SpringbootAopWeblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuurick.weblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAopWeblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
